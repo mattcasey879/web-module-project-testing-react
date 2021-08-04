4a574658b28235d9e1e7c05643960e867332418b
Matt <matthew-casey@lambdastudents.com>
2021-08-04 18:14:19 -0500

    finished what i could

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,39 +1,67 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
+import Loading from '../Loading';
 
 const testShow = {
     name: '',
     summary: '',
     seasons: [{
-        id: null,
-        name: '',
+        id: 1,
+        name: 'Season 1',
         episodes: []
+    },{
+        id: 2,
+        name: 'Season 2',
+        episodes: [{}]
     }]
     //add in approprate test data structure here.
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+    render(<Show show={testShow} selectedSeason='none'/>)
 });
 
 test('renders Loading component when prop show is null', () => {
+    const {rerender} = render(<Show show={null} />)
+    rerender(<Loading/>)
+
 });
 
-test('renders same number of options seasons are passed in', ()=>{
+test('renders same number of options seasons are passed in', async ()=>{
+    render(<Show show={testShow} selectedSeason='none'/>)
+    const seasons = screen.getAllByTestId('season-option')
+    expect(seasons).toHaveLength(2)
 });
 
 test('handleSelect is called when an season is selected', () => {
+    const fakeHandleSelect = jest.fn(() => {
+        return 'Fake handle Select';
+    });
+
+    render(<Show show={testShow} selectedSeason='none' handleSelect={() => fakeHandleSelect('fake')}/>)
+
+    const select = screen.queryAllByTestId('season-option')
+    userEvent.selectOptions(select[0], '1')
+    expect(fakeHandleSelect).toHaveBeenCalled()
+    expect(fakeHandleSelect.mock.calls.length).tobe(1)
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+    const { rerender } = render(<Show show={testShow} selectedSeason='none'/>)
 });
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
+
 //2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
+
 //3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
+
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
+
 //6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
